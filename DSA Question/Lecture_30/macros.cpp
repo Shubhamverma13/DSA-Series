c6030652999d7c716196443b66a867ebc5a24afd
shubhamverma-code <shubhamverma9454354170@gmail.com>
2023-01-29 22:45:43 +0530

    macros concept

---
+++ DSA Question/Lecture_30/macros.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define PI 3.14
+int main()
+{
+    int r = 5;
+    double area = PI*r*r;//does not need memory allocate and run time is less
+    //koi change nmai kr sakta value
+    // memory space alag se lene ka koi need nai hai
+    cout<<"Area is " <<area<<endl;
+}
